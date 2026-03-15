4ea78cbd6a7fb2a5c6b7b56827d4e0d717812124
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-03-15 18:30:29 +0100

    feat: added global background styles and basic header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import styles from "./page.module.css";
 import { Marquee } from "@/components/Marquee/Marquee";
+import { Header } from "@/components/Header/Header";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Header />
       <Marquee />
     </div>
   );
