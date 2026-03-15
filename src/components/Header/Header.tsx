de4b2ba76fd6e6c2ed3d7a081e8ec52346130b5e
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-03-15 21:39:17 +0100

    styles: added animated bg, added photo to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
 import styles from "./Header.module.css";
+import Image from "next/image";
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <h1 className={styles.title}>Welcome!</h1>
+            <div className={styles.titleContainer}>
+                <Image src="/do_cv.jpg" alt="Profile picture" width={100} height={100} className={styles.profileImage} />
+                <h1 className={styles.title}>Welcome!</h1>
+            </div>
             <p className={styles.subtitle}>Let me show you my work.</p>
         </header>
     )
