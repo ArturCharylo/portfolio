c1a55ed48b80fbaf22a9e7e85b830fc9bb37a385
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-03-17 11:27:39 +0100

    styles: added styles for projects intro text

--- src/components/ProjectIntro/ProjectIntro.tsx
+++ src/components/ProjectIntro/ProjectIntro.tsx
@@ -3,8 +3,10 @@ import styles from './ProjectIntro.module.css'
 export function ProjectIntro() {
   return (
     <div className={styles.projectIntro}>
-      <h2>Projects</h2>
-      <p>Explore a selection of my projects, showcasing my skills and creativity in various domains. Each project highlights unique challenges and innovative solutions, reflecting my passion for technology and design.</p>
+      <h2 className={styles.description}>Products</h2>
+      <p className={styles.description}>
+        Explore a selection of my products, showcasing my skills and creativity in various domains. Each product highlights unique challenges and innovative solutions, reflecting my passion for technology and design.
+      </p>
     </div>
   );
 }
