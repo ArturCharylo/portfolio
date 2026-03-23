fc05371047a60aab942f889851313baa4aff4df9
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-03-23 12:45:33 +0100

    deleted titles for sections and updates about styles

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,8 +4,7 @@ export const About = () => {
     return (
         <>
             <div className={styles.about}>
-                <h1>About</h1>
-                <p>
+                <p className={styles.description}>
                     This is a simple React application that demonstrates the use of React Router for navigation and SCSS for styling. It includes a home page, an about page, and a contact page. The application is designed to be responsive and user-friendly.
                 </p>
             </div>
--- src/components/ProjectIntro/ProjectIntro.tsx
+++ src/components/ProjectIntro/ProjectIntro.tsx
@@ -3,7 +3,6 @@ import styles from './ProjectIntro.module.css'
 export function ProjectIntro() {
   return (
     <div className={styles.projectIntro}>
-      <h2 className={styles.description}>Products</h2>
       <p className={styles.description}>
         Explore a selection of my products, showcasing my skills and creativity in various domains. Each product highlights unique challenges and innovative solutions, reflecting my passion for technology and design.
       </p>
