295be30be2408eac4b39a15e9c81b1e20f7f935b
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-03-31 15:03:23 +0200

    styles: added basic styles to projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,8 @@
+import styles from './projects.module.css';
+
 export default function Projects() {
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Projects Page</h1>
             <p>This is where the projects will be listed.</p>
         </div>
