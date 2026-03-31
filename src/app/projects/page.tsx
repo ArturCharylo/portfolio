e1eeeb63c2b40cd5ba48bdcc8e4b57facc602524
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-03-31 14:42:25 +0200

    feat: made the link to projects page work

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+    return (
+        <div>
+            <h1>Projects Page</h1>
+            <p>This is where the projects will be listed.</p>
+        </div>
+    );
+}
